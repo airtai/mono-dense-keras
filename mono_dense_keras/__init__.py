d2f41d6ad9e7b39abc227fee7e9dc82255752f5d
Davor Runje <davor@airt.ai>
2023-05-26 14:17:56 +0000

    polishing

--- mono_dense_keras/__init__.py
+++ mono_dense_keras/__init__.py
@@ -5,7 +5,10 @@ __version__ = "0.0.6rc0"
 __all__ = ['dummy']
 
 # %% ../nbs/TopLevel.ipynb 1
-from ._components.mono_dense_layer import MonoDense
+from mono_dense_keras._components.mono_dense_layer import (
+    MonoDense,
+    replace_kernel_using_monotonicity_indicator,
+)
 
 # %% ../nbs/TopLevel.ipynb 2
 def dummy() -> None:
@@ -15,4 +18,4 @@ def dummy() -> None:
 dummy.__module__ = "_dummy"
 
 # %% ../nbs/TopLevel.ipynb 3
-__all__ = ["MonoDense"]
+__all__ = ["MonoDense", "replace_kernel_using_monotonicity_indicator"]
